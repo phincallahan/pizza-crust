a994d101e474c5ecec0dc53da280b88a0d40ff6e
phincallahan <phincallahan14@gmail.com>
2016-04-09 16:09:16 -0500

    created some more files

--- pizza_senpai.py
+++ pizza_senpai.py
@@ -1,20 +1,19 @@
-#!/usr/bin/env python
 
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('order', nargs='+', help='user\'s order')
-args = parser.parse_args()
-confirmation = ['order']
-crust = ['hand-tossed']
-size = ['small', 'medium', 'large']
-sauce = ['robust inspired tomato sauce']
-toppings = ['cheese', 'pepperoni', 'sausage']
-options = [confirmation, crust, size, sauce, toppings]
-string_options = ['confirmation', 'crust', 'size', 'sauce', 'toppings']
-final_order = {}
-for option in options:
-	for word in option:
-		if((string_options[options.index(option)] != 'confirmation') and (word in args.order)):
-			final_order[string_options[options.index(option)]] = word
-print final_order 
+def sentence_order(options):
+    confirmation = ['order']
+    crust = ['hand-tossed']
+    size = ['small', 'medium', 'large']
+    sauce = ['robust inspired tomato sauce']
+    toppings = ['cheese', 'pepperoni', 'sausage']
 
+    options = [confirmation, crust, size, sauce, toppings]
+    string_options = ['confirmation', 'crust', 'size', 'sauce', 'toppings']
+
+    final_order = {}
+
+    for option in options:
+        for word in option:
+            if((string_options[options.index(option)] != 'confirmation') and (word in args.order)):
+                final_order[string_options[options.index(option)]] = word
+
+    return final_order
