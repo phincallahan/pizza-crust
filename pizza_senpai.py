157d10e5adb514132d8d78f7d8495cfd19a9cc88
die-bold <a.s.diebold@gmail.com>
2016-04-10 12:42:27 -0500

    Added files via upload

--- pizza_senpai.py
+++ pizza_senpai.py
@@ -1,21 +1,21 @@
-def sentence_order(args):
-	crust = ['hand-tossed']
-	size = ['small', 'medium', 'large']
-	sauce = ['garlic parmesan white sauce', 'alfredo sauce', 'bbq sauce', 'robust inspired tomato sauce', 'hot sauce', 'hearty marinara sauce']
-	toppings = ['ham', 'beef', 'spinach', 'banana peppers', 'feta cheese', 'premium chicken', 'salami', 'green olives', 'roasted red peppers', 'philly steak', 'shredded provolone cheese', 'italian sausage', 'pepperoni', 'pineapple', 'jalapeno peppers', 'cheese', 'green peppers', 'cheedar cheese', 'shredded parmesan asiago', 'american cheese', 'bacon', 'diced tomatoes', 'mushrooms', 'black olives', 'onions', 'sliced italian sausage']
-	#options = [crust, size, sauce, toppings]
-	#string_options = ['crust', 'size', 'sauce', 'toppings']
-	options = [size, sauce, toppings]
-	string_options = ['size', 'sauce', 'toppings']
+def sentence_order(args):   #takes a list of strings that would form the sentence ex. ['I', 'would', 'like', 'to', 'order', 'cheese', 'pizza']
+	crust = ['hand-tossed']      #possible crusts
+	size = ['small', 'medium', 'large']  #possible sizes
+	sauce = ['garlic parmesan white sauce', 'alfredo sauce', 'bbq sauce', 'robust inspired tomato sauce', 'hot sauce', 'hearty marinara sauce'] #possible sauces
+	toppings = ['ham', 'beef', 'spinach', 'banana peppers', 'feta cheese', 'premium chicken', 'salami', 'green olives', 'roasted red peppers', 'philly steak', 'shredded provolone cheese', 'italian sausage', 'pepperoni', 'pineapple', 'jalapeno peppers', 'cheese', 'green peppers', 'cheedar cheese', 'shredded parmesan asiago', 'american cheese', 'bacon', 'diced tomatoes', 'mushrooms', 'black olives', 'onions', 'sliced italian sausage'] #possible toppings
+	#options = [crust, size, sauce, toppings] #list of options, use if want different crust options
+	#string_options = ['crust', 'size', 'sauce', 'toppings']  #options list but using strings instead of lists
+	options = [size, sauce, toppings]      #list of options, use if want hand-tosses pizza no matter what
+	string_options = ['size', 'sauce', 'toppings'] #string version of options array
 
-	food_dict =  {'garlic parmesan white sauce': u'Xw', 'ham': u'H', 'beef': u'B', 'spinach': u'Si', 'banana peppers': u'Z', 'feta cheese': u'Fe', 'alfredo sauce': u'Xf', 'premium chicken': u'Du', 'bbq sauce': u'Bq', 'salami': u'Sa', 'green olives': u'V', 'roasted red peppers': u'Rr', 'philly steak': 'Pm', 'shredded provolone cheese': u'Cp', 'italian sausage': u'S', 'pepperoni': u'P', 'pineapple': u'N', 'jalapeno peppers': u'J', 'cheese': u'C', 'green peppers': u'G', 'cheddar cheese': u'E', 'shredded parmesan asiago': u'Cs', 'american cheese': u'Ac', 'bacon': u'K', 'diced tomatoes': u'Td', 'mushrooms': u'M', 'robust inspired tomato sauce': u'X', 'black olives': u'R', 'onions': u'O', 'hot sauce': u'Ht', 'hearty marinara sauce': u'Xm', 'sliced italian sausage': u'Sb'}
+	food_dict =  {'garlic parmesan white sauce': u'Xw', 'ham': u'H', 'beef': u'B', 'spinach': u'Si', 'banana peppers': u'Z', 'feta cheese': u'Fe', 'alfredo sauce': u'Xf', 'premium chicken': u'Du', 'bbq sauce': u'Bq', 'salami': u'Sa', 'green olives': u'V', 'roasted red peppers': u'Rr', 'philly steak': 'Pm', 'shredded provolone cheese': u'Cp', 'italian sausage': u'S', 'pepperoni': u'P', 'pineapple': u'N', 'jalapeno peppers': u'J', 'cheese': u'C', 'green peppers': u'G', 'cheddar cheese': u'E', 'shredded parmesan asiago': u'Cs', 'american cheese': u'Ac', 'bacon': u'K', 'diced tomatoes': u'Td', 'mushrooms': u'M', 'robust inspired tomato sauce': u'X', 'black olives': u'R', 'onions': u'O', 'hot sauce': u'Ht', 'hearty marinara sauce': u'Xm', 'sliced italian sausage': u'Sb'} #dictionary of unicode for toppings and sauces that the api needs
 
-	final_order = {}
+	final_order = {} #dictionary that will return toppings/size/sauce as the key with the user's choice as the definition
 
-	for x in range(0, len(args)):
+	for x in range(0, len(args)):    #ignores case of input
 		args[x] = args[x].lower()
 
-	for x in range(0, len(args)):
+	for x in range(0, len(args)):       #concatenates input so multi-word things are put together ex. 'bbq' + 'sauce' = 'bbq sauce'
 		if args[x] == 'sauce':
 			args[x-1] += ' ' + args[x]
 			args[x] = 'xxxxx'
@@ -56,22 +56,22 @@ def sentence_order(args):
 
 
 
-	for option in options:
-		for word in option:
-			if word in args:
-				if word in food_dict.keys():
-					final_order[string_options[options.index(option)]] = food_dict[word]
-				else:
-					final_order[string_options[options.index(option)]] = word
-	#final_order['crust'] = 'hand-tossed'
-	if 'size' not in final_order.keys():
+	for option in options:  #cyles through options list
+		for word in option:  #cycles through elements in list
+			if word in args:  #checks if element is in the use input list
+				if word in food_dict.keys():  #checks if element is in the dictionary
+					final_order[string_options[options.index(option)]] = food_dict[word] #adds dictionary entry with topping/sauce/size as key and unicode as definition
+				else: #element is not in dictionary
+					final_order[string_options[options.index(option)]] = word #adds dictionary entry with topping/sauce/size as key and string of word ex. 'medium'
+	#final_order['crust'] = 'hand-tossed' #adds crust as entry to dictionary if eeded
+	if 'size' not in final_order.keys():	#sets default size if size not specified
 		print "You have opted for the default size or selected a non-eligible size."
-		final_order['size'] = 'medium'
-	if 'sauce' not in final_order.keys():
+		final_order['size'] = 'medium' 
+	if 'sauce' not in final_order.keys():  #sets default sauce if sauce not specified
 		print "You have opted for the default sauce or selected a non-eligible sauce."
 		final_order['sauce'] = food_dict[u'robust inspired tomato sauce']
-	if 'toppings' not in final_order.keys():
+	if 'toppings' not in final_order.keys(): #sets default topping if topping not specified
 		print "You have opted for the default topping  or selected a non-eligible topping."
 		final_order['toppings'] = food_dict[u'cheese']
 
-	return final_order
+	return final_order #returns dictionary ex. {'topping': 'cheese', 'sauce': u'Xu', 'size': 'medium'}
